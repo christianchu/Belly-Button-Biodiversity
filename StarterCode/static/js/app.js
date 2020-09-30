963fb8e9962a71885dc1ae8e10125ecea19ca681
Christian Chu <66145369+christianchu@users.noreply.github.com>
2020-09-29 21:49:34 -0700

    add

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,19 +2,15 @@
 d3.json("samples.json").then((bellyData) => {
     console.log(bellyData);
 
-    // filter sample values by id 
     var samples = bellyData.samples.filter(bellyData => bellyData.id.toString() === id)[0];
 
     var sampleValues = samples.sample_values.slice(0, 10).reverse();
 
-    // get only top 10 otu ids for the plot
     var idValues = (samples.otu_ids.slice(0, 10)).reverse();
 
-    // get the otu id's to the desired form for the plot
     var idOtu = idValues.map(d => "OTU " + d)
 
     var labels = idValues.map(d => "OTU " + d)
-
 
     var trace = {
         x: sampleValues,
